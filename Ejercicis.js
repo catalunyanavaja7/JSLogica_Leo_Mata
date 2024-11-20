588af05a9abc8a87ec1d7bcc83174de411d13a5b
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-20 14:09:22 +0100

    Ejercicio 2

--- Ejercicis.js
+++ Ejercicis.js
@@ -11,3 +11,16 @@ if (multiplo % 3 == 0) {
 }else {
     console.log("No es multiplo de ninguno")
 }
+
+// Ejercicio 2
+console.log("=====Ejercicio 2=====")
+
+let x = 20;
+let y = 55;
+
+let ternaria = x >= 10 && x <= 50 ? x + " está entre 10 y 50" : x + " no está entre 10 y 50"
+console.log(ternaria)
+
+let ternaria2 = y >= 10 && y <= 50 ? y + " está entre 10 y 50" : y + " no está entre 10 y 50"
+console.log(ternaria2)
+
