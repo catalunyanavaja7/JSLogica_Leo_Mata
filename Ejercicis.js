39835e41554be39dedee2a3cd29335a9f30c66fe
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-20 13:56:27 +0100

    Ejercicio 1

---
+++ Ejercicis.js
@@ -0,0 +1,13 @@
+// Ejercicio 1
+console.log("=====Ejercicio 1=====")
+
+var multiplo = 21;
+
+if (multiplo % 7 == 0) {
+    console.log(multiplo + " es multiplo de 7")
+}
+if (multiplo % 3 == 0) {
+    console.log(multiplo + " es multiplo de 3")
+}else {
+    console.log("No es multiplo de ninguno")
+}
